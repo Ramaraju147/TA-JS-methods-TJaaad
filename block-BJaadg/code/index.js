d7653e4d80719b8c02c76ab985bafc012b4fb830
jvegiraju <raju.vegiraju18@gmail.com>
2020-12-02 06:02:43 -0500

    Array and Srting Methods

--- block-BJaadg/code/index.js
+++ block-BJaadg/code/index.js
@@ -14,7 +14,7 @@ let indexOfIs = quote.indexOf("is");
 2. Find the character at the index indexOfIs (Problem 1) in quote.
 */
 
-quote(indexOfIs);
+quote.charAt(indexOfIs);
 
 /*
 3. Log the message saying `The index of first is in quote is 7`
