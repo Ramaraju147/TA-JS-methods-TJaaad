15bfdced2e6a86d1095d71ac57c383672c17e703
Janakiramaraju Vegiraju <Janakiramaraju.X.Vegiraju@kp.org>
2021-12-20 01:15:53 -0500

    Array and String Methods

--- block-BJaadg/code/index.js
+++ block-BJaadg/code/index.js
@@ -8,7 +8,8 @@ let to = "Arya Stark";
 1. Find the index of the first 'is' in the variable quote. And store it in a new variable named indexOfIs
 */
 
-let indexOfIs = quote.indexOf("is");
+let indexOfIs = quote.indexOf( 'is' );
+
 
 /*
 2. Find the character at the index indexOfIs (Problem 1) in quote.
@@ -16,11 +17,14 @@ let indexOfIs = quote.indexOf("is");
 
 quote.charAt(indexOfIs);
 
+
+
 /*
 3. Log the message saying `The index of first is in quote is 7`
 */
+console.log( `The index of first is in quote is ${indexOfIs}` )
 
-console.log(`The index of first is in quote is ${indexOfIs}`);
+
 
 /*
 4. Log the message for first 6 characters of quote like this.
@@ -32,66 +36,81 @@ console.log(`The index of first is in quote is ${indexOfIs}`);
   The character at index 5 is ' '
 */
 
-let firstSix = quote.slice(0, 5);
+let firstSixChar = quoate.slice( 0, 5 );
 
-for (i of firstSix) {
-  console.log(`The character at index ${firstSix.indexOf(i)} is '${i}'`);
+for ( let i = 0; i < firstSixChar.length - 1; i++ ) {
+  console.log( `The character at index ${i} is ${firstSixChar[i]} ` )
 }
+
+
 
 /*
 5. Using the variable from , to and quote variable dispaly this message
   "Syrio Forel said There is only one thing we say to death: Not today to Arya Stark." (use concat method)
 */
 
-"".concat(from, " ", quote, " ", to);
+"".concat( from, " ", quote, " to", to );
 
 /*
 6. Does from, to and quote ends with "rk". Check all three.
 */
-[from, quote, to].every((x) => x.endsWith("rk"));
+
+[from, to, quote].every( ( str ) => str.endsWith( "rk" ) );
+
 
 /*
 7. Does quote includes the word "Only"
 */
+
 quote.includes("Only");
 
 /*
 8. Does quote includes the word " we"
 
 */
-quote.includes(" we");
+
+quote.includes( " we" )
+
 /*
 9. Find the index of the the word `we` in quote
 */
-quote.index("we");
+
+quote.indexOf( "we" );
+
 /*
 10. Split the quote into individual word and store it in a variable name quoteSplitted
 */
 let quoteSplitted = quote.split(" ");
 
+
 /*
 11. Change the word "today" in quoteSplitted to "tomorrow" and join all the words to form a sentance.
 */
-let indexOfToday = quoteSplitted.indexOf("today");
-quoteSplitted.splice(indexOfToday, 1, "tomorrow");
-quoteSplitted.join(" ");
+quoteSplitted.replace( "today", "tomorrow" ).join( " " );
+
 
 /*
 12. Find the index of second "o" in quote. Use indexOf
 */
+let firstOccurence = quote.indexOf( "o" );
 
-quote.indexOf("o", quote.indexOf("o") + 1);
+console.log( quote.indexOf( "o", firstOccurence + 1 ) );
+
+
 
 /*
 13. Find the last index of letter "a" in quote.
 */
+
 quote.lastIndexOf("a");
+
 
 /*
 14. Find the second last index of letter "a" in quote.
 */
 
-quote.lastIndexOf("a", quote.lastIndexOf("a") - 1);
+quote.lastIndexOf( "a", quote.lastIndexOf( "a" ) - 1 );
+
 
 /*
 15. Make the quote 70 character long. If it has less characters add rest as .......
@@ -99,37 +118,45 @@ Example: "Hello" (convert to 10 characters) => "Hello....."
 Store the output in a new variable
 */
 
-quote.padEnd(70, ".");
+quote.padEnd( 70, "." )
 
 /*
 16. Do same as (15) but the ... should come in start. Store the output in a new variable
 */
 
-quote.padStart(70, ".");
+quote.padStart( 15, "." );
+
+
 
 /*
 17. Log the repeat of "Hello World!" 10 times.
 */
-console.log("Hello World!".repeat(10));
+
+"Hello World!".repeat( 10 );
 
 /*
 18. Replace today to tomorrow in quote.
 */
-quote.replace("today", "tomorrow");
+
+quoate.replace( "today", "tomorrow" );
+
 
 /*
 19. Replace Stark to Lannister in quoteTo
 */
-to.replace("Stark", "Lannister");
+
+quoteTo.replace( "Stark", "Lannister" );
 
 /*
 20. Make the quote of length 30 and put ... at the end. (use slice)
 */
 
-quote.slice(0, 30).concat("...");
+quote.padEnd( 30, "." );
 
 /*
 21. Find out does quote, from, to starts with "A"
 */
 
-[quote, to, from].every((x) => x[0] == "A");
+[quote, from, to].every( str => str.startsWith( "A" ) );
+
+
