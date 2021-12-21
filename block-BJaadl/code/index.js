86b5120ed2502431299e67b1bf0b1fa2f8b7dab8
Janakiramaraju Vegiraju <Janakiramaraju.X.Vegiraju@kp.org>
2021-12-21 02:53:01 -0500

    Assignment Level II

--- block-BJaadl/code/index.js
+++ block-BJaadl/code/index.js
@@ -11,81 +11,81 @@ let words = [
 
 // - Write a function findLongestWord that takes an array of words and returns the longest word from the array. (Use above array "words" to test it). If there are 2 with the same length, it should return the first occurrence.
 
-let longestWord = words.sort((a, b) => a.length - b.length).pop();
+function findLongestWord(arr){
+  return [...arr].sort((a,b) => b.length - a.length).shift()
+}
 
 // - Convert the above array "words" into an array of length of word instead of word.
-longestWord.length;
+
+let wordsLength = words.map(w => w.length);
+
 
 // - Create a new array that only contains word with atleast one vowel.
-let vowel = ["a", "e", "i", "o", "u"];
-words.filter((w) => {
-  for (let v of vowel) {
-    if (w.includes(v)) {
-      return true;
-    }
+
+let vowels = ['a','e','i','o','u'];
+
+let vowelWords = words.filter((word) => {
+  for( let i of vowels){
+    return word.includes(i)
   }
-});
+} )
 
 // - Find the index of the word "rhythm"
-words.indexOf("rythm");
+let index = words.find((word,i) => {
+  if(word === "rhythm"){
+    return i;
+  }
+})
+
 
 // - Create a new array that contians words not starting with vowel.
-words.filter((w) => {
-  for (let v of vowel) {
-    if (!w.startsWith(v)) {
-      return true;
-    }
-  }
-});
+let newArray = words.filter(word => {
+  return vowels.every(vowel => vowel != word[0]);
+})
+
+console.log(newArray);
+
 
 // - Create a new array that contianse words not ending with vowel
-words.filter((w) => {
-  for (let v of vowel) {
-    if (!w.endsWith(v)) {
-      return true;
-    }
-  }
-});
+
+let notVowelEndings = words.filter(word => {
+  return vowels.every(vowel => vowel != word[word.length-1]);
+})
+
+console.log(notVowelEndings);
+
 
 let numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
 // - Create a sumArray function that takes an array of number as a parameter, and calculate the sum of all its numbers
 
-function sumArray(acc, number) {
-  return acc + number;
-}
-numbers.reduce(sumArray);
+let sumArray = numbers.reduce((acc,num) => acc+num,0);
+console.log(sumArray);
 
 // - Make a new array that contains number multiplied by 3 like [6, 18, 27 ...]
-numbers.map((number) => number * 3);
+let threeMultiples = numbers.map(num => num*3);
 
 // - Create a new array that contains only even numbers
-numbers.filter((number) => number % 2 == 0);
+let evenNum = numbers.filter(num => num%2==0);
 
 // - Create  a new array that contains only odd numbers.
-numbers.filter((number) => number % 2 !== 0);
+let oddNum = numbers.filter(num => num%2!=0);
+
 
 // - Create a new array that should have true for even number and false for odd numbers.
-numbers.map((number) => number % 2 == 0);
+let booleanArr = numbers.map(num => num%2==0?true:false);
 
 // - Sort the above number in assending order.
-numbers.sort((a, b) => a - b);
+let ascNum = numbers.sort((a,b) => a-b);
 
 // - Does sort mutate the original array?
-yes;
+yes
 
 // - Find the sum of the numbers in the array.
-numbers.reduce((acc, num) => acc + num);
+let sum = numbers.reduce((acc,num) => acc+num,0)
 
 //- Write a function averageNumbers that receives an array of numbers and calculate the average of the numbers
-
-function averageNumbers(arr) {
-  let sum = arr.reduce((acc, num) => acc + num);
-  let avg = sum / arr.length;
-  return avg;
-}
-
-averageNumbers(numbers);
+let average = sum/(numbers.length)
 
 let strings = [
   "seat",
@@ -102,8 +102,7 @@ let strings = [
 
 // - Write a function averageWordLength that receives an array of words2 and calculate the average length of the words.
 
-function averageWordLength(str) {
-  let sum = str.reduce((acc, st) => acc + st.length, 0);
-  console.log(sum);
-  return sum / str.length;
-}
+
+let WordLength = strings.reduce((acc,str) => acc+str.length,0)
+
+let averageWordLength = WordLength/strings.length
